c598c09b01297555e577fc115c210ee800124808
Anouar-91 <a.berrouane@hotmail.com>
2022-12-15 23:10:57 +0100

    user login

--- src/redux-toolkit/reducers/userReducer.js
+++ src/redux-toolkit/reducers/userReducer.js
@@ -1,17 +1,48 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const login = createAsyncThunk("user/login", async (auth) => {
+  const {email, password} = auth;
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  }
+  try {
+    const { data } = await axios.post(process.env.REACT_APP_API_URL + 'users/login', {
+      email, password
+    }, config)
+    return data;
+  } catch (error) {
+    return error.response && error.response.data.message
+      ? error.response.data.message
+      : error.message
+  }
+});
 
 export const userLoginSlice = createSlice({
-    name: "userLogin",
-    initialState: {
+  name: "userLogin",
+  initialState: {
+  },
+  reducers: {
+    logout: (state, { payload }) => {
+      state = {}
     },
-    reducers: {
-      login: (state, { payload }) => {
-        state = {userInfo: payload}
-      },
-      logout: (state, { payload }) => {
-        state = {}
-      },
-    },
-  });
-  
-  export const { login,logout } = userLoginSlice.actions;
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(login.pending, (state) => {
+        return { loading: true };
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        const {_id} = action.payload
+        if(_id){
+          return  { loading: false, userInfo: action.payload };
+        }else{
+          return {error:action.payload }
+        }
+      })
+  },
+});
+
+export const { logout } = userLoginSlice.actions;
---
+++ src/screens/LoginScreen.jsx
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+import { login } from '../redux-toolkit/reducers/userReducer';
+import FormContainer from '../components/FormContainer';
+
+
+const LoginScreen = ({ }) => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    let location = useLocation();
+    const dispatch = useDispatch();
+    const userLogin = useSelector(state => state.userLogin);
+    const {loading , error, userInfo} = userLogin;
+    const navigate = useNavigate();
+
+    const redirect = location.search ? location.search.split('=')[1] : '/'
+    const submitHandler = (e) => {
+        e.preventDefault();
+        dispatch(login({email, password}))
+    }
+
+    useEffect(() => {
+        if(userInfo){
+            navigate(redirect)
+        }
+    }, [dispatch, userInfo, redirect])
+    return (
+        <div>
+            <FormContainer>
+                <h1>Sign In</h1>
+                {error && <Message variant="danger">{error}</Message> }
+                {loading && <Loader/>}
+                <form onSubmit={submitHandler} action="" className="mt-4">
+                    <div className="form-group">
+                        <label htmlFor="email">Email Address</label>
+                        <input id='email' name="email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter your email address" className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Password</label>
+                        <input id='password' name="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter your password" className="form-control" />
+                    </div>
+                    <button className="btn btn-primary mt-3">Sign in</button>
+                </form>
+                <div className="row mt-3">
+                    <div className="col">
+                        New customer ? <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>Register</Link>
+                    </div>
+                </div>
+            </FormContainer>
+        </div>
+    )
+}
+
+export default LoginScreen
