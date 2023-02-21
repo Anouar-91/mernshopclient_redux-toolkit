7257e3ef3126306431c3b7dfa3d832b0222d71df
Anouar-91 <a.berrouane@hotmail.com>
2023-02-21 10:31:38 +0100

    fix bug shippingAddress

--- src/screens/ShippingScreen.jsx
+++ src/screens/ShippingScreen.jsx
@@ -19,7 +19,11 @@ const ShippingScreen = () => {
     const submitHandler = (e) => {
         e.preventDefault();
         dispatch(saveShippingAddress({ address, city, country, postalCode }))
-        navigate("/payment")
+        if(address === "" || city ==="" || country ==="" || postalCode === ""){
+            alert('You must fill all the fields')
+        }else{
+            navigate("/payment")
+        }
     }
     return (
         <FormContainer>
@@ -31,19 +35,19 @@ const ShippingScreen = () => {
             <form onSubmit={submitHandler} action="">
                 <div className="form-group">
                     <label htmlFor="address">address</label>
-                    <input id='address' name="address" type="text" value={address} onChange={(e) => setAddress(e.target.value)} placeholder="Enter your address" className="form-control" />
+                    <input required id='address' name="address" type="text" value={address} onChange={(e) => setAddress(e.target.value)} placeholder="Enter your address" className="form-control" />
                 </div>
                 <div className="form-group">
                     <label htmlFor="city">city</label>
-                    <input id='city' name="city" type="text" value={city} onChange={(e) => setCity(e.target.value)} placeholder="Enter your city" className="form-control" />
+                    <input required id='city' name="city" type="text" value={city} onChange={(e) => setCity(e.target.value)} placeholder="Enter your city" className="form-control" />
                 </div>
                 <div className="form-group">
                     <label htmlFor="postalCode">postalCode</label>
-                    <input id='postalCode' name="postalCode" type="text" value={postalCode} onChange={(e) => setPostalCode(e.target.value)} placeholder="Enter your postalCode" className="form-control" />
+                    <input required id='postalCode' name="postalCode" type="text" value={postalCode} onChange={(e) => setPostalCode(e.target.value)} placeholder="Enter your postalCode" className="form-control" />
                 </div>
                 <div className="form-group">
                     <label htmlFor="country">country</label>
-                    <input id='country' name="country" type="text" value={country} onChange={(e) => setCountry(e.target.value)} placeholder="Enter your country" className="form-control" />
+                    <input required id='country' name="country" type="text" value={country} onChange={(e) => setCountry(e.target.value)} placeholder="Enter your country" className="form-control" />
                 </div>
                 <div className="text-center">
                     <button className="btn btn-primary mt-4">Continue</button>
